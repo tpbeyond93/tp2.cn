836a383878a23617c4ed483d834155d26ec897ed
tangpeng <1048440489@qq.com>
2022-01-17 11:24:54 +0800

    入口文件修改2

--- public/index.php
+++ public/index.php
@@ -9,9 +9,7 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-// [ 后台入口文件 ]
-// 使用此文件可以达到隐藏admin模块的效果
-// 为了你的安全，强烈不建议将此文件名修改成admin.php
+// [ 应用入口文件 ]
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 
@@ -22,16 +20,4 @@ if (!is_file(APP_PATH . 'admin/command/Install/install.lock')) {
 }
 
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/base.php';
-
-// 绑定到admin模块
-\think\Route::bind('admin');
-
-// 关闭路由
-\think\App::route(false);
-
-// 设置根url
-\think\Url::root('');
-
-// 执行应用
-\think\App::run()->send();
+require __DIR__ . '/../thinkphp/start.php';
